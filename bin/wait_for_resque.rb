7439e7fa319e68d80c52bb593e103a7f5911d417
xxwhirlpool <melodicake@disroot.org>
2024-12-11 17:32:59 -0500

    fix

--- bin/wait_for_resque.rb
+++
@@ -1,9 +0,0 @@
-require 'resque'
-require "/home/katmac3/superlove/otwarchive/config/environment"
-
-begin
-  info=Resque.info
-  sum=info[:working]+info[:pending]
-  puts sum
-  sleep 5
-end while sum != 0
