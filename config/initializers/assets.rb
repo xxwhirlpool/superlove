5d651521f8146a1300da79781781848686710016
Carly Ho <carlymho@fastmail.com>
2026-03-15 20:51:40 -0500

    add syntax highlighting to skin pages

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( highlightjs/atelier-cave-light.min.css )
+Rails.application.config.assets.precompile += %w( highlightjs/highlight.min.js )
+Rails.application.config.assets.precompile += %w( highlightjs/css.min.js )
