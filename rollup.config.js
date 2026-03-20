bc3faf48fad4c445ceb685eb610dc15794e5d743
Carly Ho <carlymho@fastmail.com>
2026-03-19 23:00:24 -0700

    also update config files

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,16 @@ import resolve from "@rollup/plugin-node-resolve"
 export default {
   input: [
     "app/assets/javascript/application.js",
-    "app/assets/javascript/skineditor.js"
+    "app/assets/javascript/skineditor.js",
+    "app/assets/javascript/workeditor.js",
+    "app/assets/javascript/search.js"
   ],
-  output: [
-    {
-      dir: "app/assets/builds",
-      format: "esm",
-      inlineDynamicImports: false,
-      sourcemap: true
-    },
-    {
-      dir: "app/assets/builds",
-      format: "esm",
-      inlineDynamicImports: false,
-      sourcemap: true
-    }
-  ],
+  output: {
+    dir: "app/assets/builds",
+    format: "esm",
+    inlineDynamicImports: false,
+    sourcemap: true
+  },
   plugins: [
     resolve()
   ]
