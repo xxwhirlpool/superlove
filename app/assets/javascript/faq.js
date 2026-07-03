ab52e27e6a5509b9875e55748fe0f9922b113850
Carly Ho <carlymho@fastmail.com>
2026-07-03 11:16:28 -0500

    fix pagination display, do some cleanup of the faq cat list page

--- app/assets/javascript/faq.js
+++ app/assets/javascript/faq.js
@@ -1,7 +1,10 @@
 const add_section = (elt, model, content) => {
+  const addQBtn = document.getElementById('add-question');
   const group = document.getElementById(model);
   const index = parseInt(group.querySelector('.faq-item:last-of-type').getAttribute('data-index'));
   group.innerHTML += content;
+  const onClickFxn = addQBtn.onclick;
+  addQBtn.onclick = onClickFxn.replaceAll(index.toString(), (index+1).toString());
 }
 
 document.addEventListener('click', (e) => {
